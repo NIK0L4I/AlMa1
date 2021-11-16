05f0f583844dcd29d8cf40dd5ff69390cb35fda3
Nikolai Less <s6niless@uni-bonn.de>
2021-11-16 11:42:27 +0100

    Abgabecode Blatt 04 - richtige Reihenfolge im model

--- 04_blatt/a4.py
+++ 04_blatt/a4.py
@@ -32,23 +32,25 @@ def plot_time(xscale_value):
     zu approximieren.
     '''
     time = []
-    df = pd.read_csv("Laufzeiten.csv")
+    df = pd.read_csv("laufzeiten.csv")
     x = df['n']
     y = df['Laufzeit']
+    model = np.poly1d(np.polyfit(x, y, 2))
+    polyline = np.linspace(0, xscale_value**5, 50)
+    print(model)
     plt.xlabel('Anzahl der Elemente')
     plt.ylabel('Laufzeit in s')
     plt.xscale("log", base=xscale_value)
     plt.plot(x,y,'ob',label='Laufzeit BubbleSort')
-    model = np.poly1d(np.polyfit(x, y, 2))
-    polyline = np.linspace(1, xscale_value**5, 50)
     plt.plot(polyline, model(polyline),\
         'r',label='${}x^2 + {}x + {}$'.format(\
-            round(model[0],3), round(model[1],3), round(model[2],3)))
+            round(model[2],8), round(model[1],5), round(model[0],3)))
+    #plt.plot(x,x**2,'y',label='$x^2$')
     plt.legend(loc='upper left')
     plt.savefig('./graphs/laufzeit.eps', format='eps')
     plt.show()
 
 if __name__ == '__main__':
-    main()
-    #plot_time(4)
+    #main()
+    plot_time(10)
 # %%
