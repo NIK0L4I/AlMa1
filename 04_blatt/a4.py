1431e83e5ac204fe53237b55aa4df2530906c80b
Nikolai Less <s6niless@uni-bonn.de>
2021-11-16 10:33:13 +0100

    Abgabgecode für Blatt 04

--- 04_blatt/a4.py
+++ 04_blatt/a4.py
@@ -1,11 +1,8 @@
 # %%
-from os import write
-import csv
 import timeit
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy.lib.polynomial import poly1d
-#import matplotlib.pyplot.scale
 import pandas as pd
 
 def bubbleSort(arr):
@@ -21,13 +18,10 @@ def bubbleSort(arr):
 
 def main():
     time = []
-    #with open("laufzeiten.csv", "a") as file:
     for k in range(1,9):
-        arr1 = np.random.randint(100, size=10**k)
-        t1 = timeit.timeit(lambda: bubbleSort(arr1), number=1)
+        arr1 = np.random.randint(100, size=10**k)   # Lege zufälliges Array der größe 10^k an mit Werten aus {0,...,99}
+        t1 = timeit.timeit(lambda: bubbleSort(arr1), number=1)  # Misst die Zeit, die BubbleSort für das sortieren eines Arrays der größe 10^k benötigt
         time.append(t1)
-            #writer = csv.writer(file)
-            #writer.writerow("{},{}".format(str(t3),10**k))
     plt.plot(time) 
     plt.show()
 
@@ -38,24 +32,23 @@ def plot_time(xscale_value):
     zu approximieren.
     '''
     time = []
-    df = pd.read_csv("Laufzeiten3.csv")
+    df = pd.read_csv("Laufzeiten.csv")
     x = df['n']
     y = df['Laufzeit']
     plt.xlabel('Anzahl der Elemente')
     plt.ylabel('Laufzeit in s')
-    #plt.yscale("log", base=10)
     plt.xscale("log", base=xscale_value)
     plt.plot(x,y,'ob',label='Laufzeit BubbleSort')
     model = np.poly1d(np.polyfit(x, y, 2))
-    polyline = np.linspace(1, xscale_value**8, 50)
+    polyline = np.linspace(1, xscale_value**5, 50)
     plt.plot(polyline, model(polyline),\
         'r',label='${}x^2 + {}x + {}$'.format(\
             round(model[0],3), round(model[1],3), round(model[2],3)))
     plt.legend(loc='upper left')
-    plt.savefig('./graphs/laufzeit4.eps', format='eps')
+    plt.savefig('./graphs/laufzeit.eps', format='eps')
     plt.show()
 
 if __name__ == '__main__':
-    #main()
-    plot_time(4)
+    main()
+    #plot_time(4)
 # %%
